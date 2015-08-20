6de687c748bc895acf9754c1c330e5d87c767721
AbhishekTiwari0812 <abhishektiwari08121994007@gmail.com>
2015-08-20 17:56:52 +0530

    Counting Subtrees.Done :)

--- count_bst_subtree.cpp
+++ count_bst_subtree.cpp
@@ -1,3 +1,6 @@
+//Complete implementation of BST.
+//delete node function still remains uncomplete!
+
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
@@ -6,6 +9,7 @@ typedef struct Node{
 	int value;
 	struct Node *left;
 	struct Node *right;
+	bool lies;	//for counting the subtrees in the range
 }node;
 
 template<class T>
@@ -103,24 +107,74 @@ void Tree<T>::post_order_traversal(T *root){
 }
 
 
+
+/* Count number of subtrees which lie in the given range.
+i.e. all the nodes have values<=high and >=low.*/
+//assumes children have alerady been checked if it's not a leaf!
+bool lies_in_range(node *,int ,int );
+int CountSubTree(node *Root,int low,int high){
+	int total_count=0;
+	int l_count=0,r_count=0;
+	if(Root->left!=NULL)
+		l_count=CountSubTree(Root->left,low,high);
+	if(Root->right!=NULL)
+		r_count=CountSubTree(Root->right,low,high);
+	total_count=l_count+r_count;
+	if(lies_in_range(Root,high,low))
+		total_count+=1;
+	
+	return total_count;
+}
+//assumes children have alerady been checked if it's not a leaf!
+bool lies_in_range(node *Root,int high,int low){
+	if(Root->left!=NULL && Root->right!=NULL)
+		if(!(Root->left->lies || Root->right->lies)){
+			Root->lies=false;
+			return false;
+		}
+		if(Root->value >= low && Root->value <= high){
+			Root->lies=true;	
+			return true;
+		}
+		else{ 
+			Root->lies=false;
+			return false;
+		}
+
+	if(Root->left==NULL && Root->right!=NULL){
+		Root->lies=Root->right->lies;
+		return Root->right->lies;
+	}
+	if(Root->left!=NULL && Root->right==NULL){
+		Root->lies=Root->left->lies;
+		return Root->left->lies;
+	}
+	if(Root->left==NULL && Root->right==NULL)
+		if(Root->value >=low && Root->value <=high)
+			Root->lies=true;	
+			return true;
+}
+
+
 int main(){
-	int size=10;
-	int A[size];
+	int size=7;
+	int A[]={4,2,6,1,3,5,7};
 	cout<<"Input array:"<<endl;
-	for(int i=0;i<size;i++){
+	/*for(int i=0;i<size;i++){
 		A[i]=rand()%10000;		//numbers range from 0 to 9999.
 		cout<<A[i]<<" ";
-	}
+	}*/
 	cout<<endl;
 	Tree<node> *m_tree=new Tree<node>(A[0]);		//allocated in the heap.
 	//root node must to added while calling the constructor!
 	m_tree->create_tree_from_array(A,size);
 	cout<<"\nIn order:"<<endl;
 	m_tree->in_order_traversal(m_tree->get_root());
-	cout<<"\nPre order:"<<endl;
-	m_tree->pre_order_traversal(m_tree->get_root());
-	cout<<"\nPost order:"<<endl;
-	m_tree->post_order_traversal(m_tree->get_root());
+	//cout<<"\nPre order:"<<endl;
+	//m_tree->pre_order_traversal(m_tree->get_root());
+	//cout<<"\nPost order:"<<endl;
+	//m_tree->post_order_traversal(m_tree->get_root());
+	cout<<"\nCount is:"<<CountSubTree(m_tree->get_root(),1,7);
 	cout<<endl;
 	return 0;
 
